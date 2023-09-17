ac463c6a360002b06162b15204cb3ff1372f5c24
DeanCode <ds@xxxmail.eu>
2023-09-17 18:25:34 +0200

    Added workflow

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,12 @@
 import adapter from '@sveltejs/adapter-static';
 
+const dev = process.argv.includes("dev");
+
 export default {
     kit: {
-        adapter: adapter({
-            // default options are shown. On some platforms
-            // these options are set automatically — see below
-            pages: 'docs',
-            assets: 'docs',
-            fallback: undefined,
-            precompress: false,
-            strict: true
-        })
+        adapter: adapter(),
+        paths: {
+            base: dev ? "" : process.env.BASE_PATH,
+        },
     }
 };
