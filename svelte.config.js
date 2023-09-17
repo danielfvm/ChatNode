c4e992d64334b2c678d6e27cccb73f98311bd224
DeanCode <ds@xxxmail.eu>
2023-09-17 17:56:08 +0200

    Build page

--- svelte.config.js
+++ svelte.config.js
@@ -5,8 +5,8 @@ export default {
         adapter: adapter({
             // default options are shown. On some platforms
             // these options are set automatically — see below
-            pages: 'build',
-            assets: 'build',
+            pages: 'www',
+            assets: 'www',
             fallback: undefined,
             precompress: false,
             strict: true
